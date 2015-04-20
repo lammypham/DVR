020988bf6f2f3a06062c5182d729ac25a4f29ba1
Lam Pham <phamlam92@gmail.com>
2015-04-20 02:43:23 -0500

    file Gui imports files and parses

--- src/com/company/FileBrowser.java
+++ src/com/company/FileBrowser.java
@@ -3,7 +3,11 @@ package com.company;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -16,6 +20,8 @@ public class FileBrowser extends JFrame implements ActionListener {
     private JTextField filename = new JTextField(), dir = new JTextField();
 
     private JButton open = new JButton("Open"), save = new JButton("Save");
+    private JTextField fileInput = new JTextField(15);//declare jtextfield
+    private ArrayList<Nodes> nodeList = new ArrayList();//declare nodelist
 
     public FileBrowser() {
         JPanel p = new JPanel();
@@ -40,16 +46,80 @@ public class FileBrowser extends JFrame implements ActionListener {
         // Demonstrate "Open" dialog:
         int rVal = c.showOpenDialog(FileBrowser.this);
         if (rVal == JFileChooser.APPROVE_OPTION) {
-            filename.setText(c.getSelectedFile().getName());
+            filename.setName(c.getSelectedFile().getName());
             dir.setText(c.getCurrentDirectory().toString());
+            System.out.println(filename.getName());
         }
+
         if (rVal == JFileChooser.CANCEL_OPTION) {
             filename.setText("You pressed cancel");
             dir.setText("");
         }
 
-       // System.out.println("here");
-    }
+            // Demonstrate "Open" dialog:
+            String[] token;
+            BufferedReader reader = null;
+            String line = null;
+
+            try {
+                reader = new BufferedReader(new FileReader(filename.getName()));
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            }
+
+            try {
+                while ((line = reader.readLine()) != null) {
+                    token = line.split(" ");
+                    int n1 = Integer.parseInt(token[0]);
+                    int n2 = Integer.parseInt(token[1]);
+                    int val = Integer.parseInt(token[2]);
+                    boolean flag1 = false;
+                    boolean flag2 = false;
+
+                    for (int i = 0; i < nodeList.size(); i++) {
+                        flag1 = (nodeList.get(i).getId() == n1) ? true : flag1;
+                        //get second node to see if it exits in array
+                        flag2 = (nodeList.get(i).getId() == n2) ? true : flag2;
+                    }
+
+                    if (!flag1) {
+                        Nodes newNode = new Nodes(n1);//create the node
+                        nodeList.add(newNode);
+                        flag1 = false;//set flag back to false
+                    }
+
+                    if (!flag2)//if it doesn't exit
+                    {
+                        Nodes newNode = new Nodes(n2);//create the node
+                        nodeList.add(newNode);
+                        flag2 = false;//set flag back to false
+                    }
+
+                    for (int index = 0; index < nodeList.size(); index++)//loop to node list size
+                    {
+                        if (nodeList.get(index).getId() == n1)//if id of node matches input
+                        {
+                            nodeList.get(index).setNeighbour(n2);//set neighbour
+                            nodeList.get(index).setCost(val);//set cost
+                        }
+                        if (nodeList.get(index).getId() == n2)//if id of node matches input
+                        {
+                            nodeList.get(index).setNeighbour(n1);//set neighbour
+                            nodeList.get(index).setCost(val);//set cost
+                        }
+                        System.out.println(nodeList.get(index));
+                    }
+
+
+                }
+
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
 
     class OpenL implements ActionListener {
         public void actionPerformed(ActionEvent e) {
---
+++ src/com/company/Nodes.java
@@ -0,0 +1,72 @@
+package com.company;
+
+/**
+ * Created by Lam_2 on 4/20/2015.
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Nodes //Nodes class
+{
+
+    private int id;//node id declared
+    public int[][] grid;//declare arry grid
+    ArrayList<Integer> neighbour = new ArrayList();//neighbour array list declared
+    ArrayList<Integer> cost = new ArrayList();//cost array list declared
+
+    public void setGrid(int[][] grid) {
+        this.grid = grid;
+    }
+
+    public int[][] getGrid()// method for getting the grid
+    {
+        return grid;//return the grid
+    }
+
+    public Nodes(int id) //constructor to create node
+    {
+        this.id = id;
+    }
+
+    public int getId() //getter for id
+    {
+        return id;
+    }
+
+    public void setId(int id) //setter for id
+    {
+        this.id = id;
+    }
+
+    public ArrayList<Integer> getNeighbour() //getter for neighbour array list
+    {
+        return neighbour;
+    }
+
+    public void setNeighbour(int neighbour) //setter for neighbour
+    {
+        this.neighbour.add(neighbour);
+    }
+
+    public ArrayList<Integer> getCost() //getter for cost array list
+    {
+        return cost;
+    }
+
+    public void setCost(int cost) //setter for cost
+    {
+        this.cost.add(cost);
+    }
+
+    public void gridPrint()//method to print gird
+    {
+        for (int[] index : grid) {
+            System.out.println(Arrays.toString(index));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nodes [id=" + id + ", neighbour=" + neighbour + ", cost=" + cost + "]";
+    }
+}
